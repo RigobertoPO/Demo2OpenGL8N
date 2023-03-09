23bac45df7bd00b64dd9227a88923517794053cb
RigobertoPO <rperez@cobach.edu.mx>
2023-03-09 17:50:46 -0600

    lineas que se unen

--- Demo2OpenGL/Demo2OpenGL/Lineas.cpp
+++ Demo2OpenGL/Demo2OpenGL/Lineas.cpp
@@ -1,4 +1,30 @@
 #include <gl/glut.h>
+void Iniciar() {
+	glClearColor(0, 0, 1, 1);
+	glColor3f(1.0, 0, 0);
+	glLineWidth(3.0);
+	gluOrtho2D(0, 800, 0, 600);
+}
+void dibujarLinea() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_LINES);
+	glVertex2i(100, 100);
+	glVertex2i(400, 100);
+	glColor3f(0, 1, 0);
+	glVertex2i(400, 100);
+	glVertex2i(400, 400);
+	glVertex2i(400, 400);
+	glVertex2i(100, 400);
+	glVertex2i(100, 400);
+	glVertex2i(100, 100);
+
+	glVertex2i(50, 365);
+	glVertex2i(250, 500);
+	glVertex2i(450, 365);
+	glVertex2i(250, 500);
+	glEnd();
+	glFlush();
+}
 int main(int argc, char** argv) {
 	glutInit(&argc, argv); //inicializa OpenGL
 	glutInitWindowSize(800, 600); //Tama?o de pantalla
